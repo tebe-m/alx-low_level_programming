a401133a2d1d5526c085d30648720e91e21b70c9
KeoioeK <tkmasedi@gmail.com>
2022-06-06 15:16:20 -0700

    output using printf with extra ,

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,8 +5,8 @@
  *
  * Return: Always 0 (Success)
  */
- int main(void)
+int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art");
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
