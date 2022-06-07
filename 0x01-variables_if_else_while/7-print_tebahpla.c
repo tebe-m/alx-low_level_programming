8d26f520aa063df73298fba33fd611c8c913e196
KeoioeK <tkmasedi@gmail.com>
2022-06-06 20:01:51 -0700

    removed new line

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+
+int main(void)
+
+{
+char alphabet;
+
+for (alphabet = 122 ; alphabet >= 97; alphabet--)
+{
+putchar(alphabet);
+}
+
+putchar('\n');
+
+return (0);
+
+
+}
+© 2022 GitHub, Inc.
+Term
