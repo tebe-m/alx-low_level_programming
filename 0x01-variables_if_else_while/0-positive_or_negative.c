0e75e8411b04bf832fd50c44d8d5b07bb7799057
KeoioeK <tkmasedi@gmail.com>
2022-06-06 18:20:28 -0700

    Check if num is o - or +

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - checks if number is 0, negative, or positive
+ *
+ *Return: returns 0
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n >= 0)
+	{
+		if (n == 0)
+		{
+			printf("is zero");
+		}
+		else
+		{
+			printf("is positive");
+		}
+	break;
+	}
+	else
+		printf("is negative");
+	return (0);
+}
