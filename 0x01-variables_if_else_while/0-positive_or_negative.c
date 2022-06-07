19646b484feaa619cbb0b64da49fab543934e48d
KeoioeK <tkmasedi@gmail.com>
2022-06-06 18:36:21 -0700

    added required spaces

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -22,12 +22,12 @@ int main(void)
 		}
 		else
 		{
-			printf("%i is positive\n",n);
+			printf("%i is positive\n", n);
 		}
 	}
 	else
 	{
-		printf("%i is negative\n",n);
+		printf("%i is negative\n", n);
 	}
 	return (0);
 }
