3035ab45a3cbf4d7d1370924e7b42ea1bf0d7ff6
KeoioeK <tkmasedi@gmail.com>
2022-06-06 18:34:01 -0700

    add new line at the end of each print out

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,16 +18,16 @@ int main(void)
 	{
 		if (n == 0)
 		{
-			printf("%i is zero", n);
+			printf("%i is zero\n", n);
 		}
 		else
 		{
-			printf("%i is positive",n);
+			printf("%i is positive\n",n);
 		}
 	}
 	else
 	{
-		printf("%i is negative",n);
+		printf("%i is negative\n",n);
 	}
 	return (0);
 }
