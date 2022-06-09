3b9a05c450b382840aeff5686292affa41b5442d
KeoioeK <tkmasedi@gmail.com>
2022-06-09 15:08:20 -0700

    checks for uppercase character.

---
+++ 0x04-more_functions_nested_loops/_isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *_isupper - checks if char is upper case or lower case
+ *
+ * @c: fucton parameter
+ * Returns: 1 if true 0 if false
+ */
+int _isupper(int c)
+{
+	(c >= 65 && c <= 90 ) ? return (1) : return (0);
+}
