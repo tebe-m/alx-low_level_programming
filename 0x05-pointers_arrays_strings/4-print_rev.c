2b5608cdb89b1c1ef1dd7fb34e932d81859488d5
Tebello Masedi <71129626+KeoioeK@users.noreply.github.com>
2022-06-15 05:46:59 +0200

    changed whitepaces as recommended by betty

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,11 +1,11 @@
 #include "main.h"
+
 /**
  * print_rev - prints a string in reverse order
  *@s: the string to be printed
  *
  *Return: nothing is returned
  */
-
 void print_rev(char *s)
 {
 	int pos = 0;
