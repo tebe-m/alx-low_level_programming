d5b00dd626d91a0d1b24910d06c5d8fa1f9002ae
KeoioeK <tkmasedi@gmail.com>
2022-06-17 01:46:49 -0700

    generates random valid passwords

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,15 +1,17 @@
 #include "main.h"
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+
 /**
+ *main - generates random valid passwords for the program 
+ *
+ *Return: returns 0 on success 
  */
 int main(void)
 {
-int num;
+	int num;	
 
-srand(time(0));
-num = rand();
-printf("%i\n", num);
-return (0);
+	srand(time(0));
+	num = rand();
+	printf("%d\n", num);
+
+	return (0);
 }
